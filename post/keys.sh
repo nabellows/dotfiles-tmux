39b850e3225a224edad34d1b2f5d547a6b2bd8ad
Nathan Bellows <nbellows@amazon.com>
2025-06-03 14:45:55 -0700

    Improve lazygit nvim-term popup
    Closer to parity with other lazygit popup

--- post/keys.sh
+++ post/keys.sh
@@ -6,6 +6,7 @@ bind() {
 
 POPUP="$TMUX_SCRIPTS_PATH/popup"
 POPUP_NVIM_TERM="$TMUX_SCRIPTS_PATH/popup-nvim-term"
+POPUP_WIN_SESH="tmux-popup-win-#{window_id}"
 
 # Currently hacked with iTerm remap C-' to <m-;> ... note that it is double quoted here in the sh-syntax area so that it is actually quoted when subbed in tmux heredocs
 PREFIX='M-\;'
@@ -58,13 +59,15 @@ bind "C-Space" run -b "#{@fzf_scripts}/keybinding.sh"
 bind "Space" show-wk-menu-root
 
 bind "$PREFIX" run "$POPUP_NVIM_TERM"
-# Still prefer this version as its marginally more responsive and I don't have much use for nvim-in-nvim
+# Still prefer this version as its marginally more responsive and I don't have much use for nvim-in-nvim (lazygit is solid, don't really need persistence for most workflows)
 # Also, the embedded/remote editing is broken in the other for now (TODO, implement)
+# Update: I added hella hacks to the nvim version so the embedded/remote editing is equivalent. Only gap right now is feels *slightly* more stuttery in nvim
 bind C-g run "$TMUX_SCRIPTS_PATH/lazygit-popup"
-bind g run "$POPUP_NVIM_TERM lazygit"
+# Somewhat convoluted, can perhaps add all the lazygit setup into core popup. POPUP_KEY not needed but helps nvim-session-args figure it out
+bind g run "$TMUX_SCRIPTS_PATH/lazygit-popup nvim-term-session $POPUP_WIN_SESH-lazygit lazygit"
 bind b run "$POPUP_NVIM_TERM btm"
 bind C-t run "$POPUP_NVIM_TERM"
-bind C-n run "$POPUP -T nvim nvim-session tmux-popup-win-#{window_id}-nvim"
+bind C-n run "$POPUP -T nvim nvim-session $POPUP_WIN_SESH-nvim"
 bind C-m run "POPUP_KEY=spt $POPUP_NVIM_TERM spotify_player"
 
 function bind_escapable() {
@@ -211,5 +214,5 @@ bind -T copy-mode-vi y send-keys -X copy-selection-and-cancel
 bind -T copy-mode-vi '[' send-keys -X previous-prompt
 bind -T copy-mode-vi ']' send-keys -X next-prompt
 
-_TMUX_SOURCE_INSTANT_KEYS=1 source "$HOME/.zshkeys"
+_TMUX_SOURCE_INSTANT_KEYS=1 . "$HOME/.zshkeys"
 
